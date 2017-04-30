528ac18f57c8bded407a7ceeb84e925a585b1705
pooria <pooria.cpp@gmail.com>
2017-04-30 14:08:50 +0430

    users,beneficiaries & back log database migrations. + laravel auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,23 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->integer('user_id')->unique();
+            $table->string('api_token');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('account_number');
+            $table->string('mobile');
+            $table->enum('sex', ['Mr.', 'Ms.', 'Co.']);
+            $table->enum('identity_number', ['passport', 'national', 'registration', 'national_id']);
+            $table->string('address');
+            $table->string('place_of_birth');
+            $table->string('date_of_birth');
+            $table->string('postal_code');
+            $table->string('tel');
+            $table->string('fax');
             $table->string('email')->unique();
             $table->string('password');
+            $table->boolean('is_authorized');
             $table->rememberToken();
             $table->timestamps();
         });
