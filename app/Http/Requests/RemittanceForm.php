8fb79b01c7c68bf9a43c069ebe77baf8a8765f0d
pooria <pooria.cpp@gmail.com>
2017-05-28 15:49:48 +0430

    debug the RemittanceForm request

--- app/Http/Requests/RemittanceForm.php
+++ app/Http/Requests/RemittanceForm.php
@@ -34,8 +34,8 @@ class RemittanceForm extends FormRequest
      *
      * @return array
      */
-    public function messages() //todo: maybe it should be delete, and add to lang validation file (for ability to define other language error msgs)
-    {
+//    public function messages() //todo: maybe it should be delete, and add to lang validation file (for ability to define other language error msgs)
+//    {
 
 //        return [
 //            'captcha.required' => 'Captcha Required!',
@@ -44,5 +44,5 @@ class RemittanceForm extends FormRequest
 //            'amount.min'  => "Please Enter an Valid Amount.",
 //            'amount.between' => "hehehe"
 //        ];
-    }
+//    }
 }
