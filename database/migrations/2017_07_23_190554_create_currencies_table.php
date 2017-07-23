56a54c99780119fa3b13456db1668353a9c2e068
pooria <pooria.cpp@gmail.com>
2017-07-23 21:10:46 +0430

    exchanger migrations , relations. dynamic redirection after first time login or signup

---
+++ database/migrations/2017_07_23_190554_create_currencies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCurrenciesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('currencies', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+
+            $table->increments('id')->unsigned();;
+            $table->enum('type',['EUR','TRY', 'USD'])->default('EUR');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('currencies');
+    }
+}
